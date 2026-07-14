8dff9d5c92aeb58397c96c5c802eab1cd5d24bce
dineshranjan2001 <dineshranjanbiswal70@gmail.com>
2026-07-14 17:06:21 +0530

    try to deploy the react application i github.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'redux-toolkit-learning',
+  base:'/redux-toolkit-learning/',
   plugins: [react(),tailwindcss()],
 })
